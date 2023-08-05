88a9111e6a0d3c048a0fcaef8f66e5d82b6b4f37
kate <katejohn967@gmail.com>
2023-08-05 11:52:22 +0100

    Book listings application

---
+++ force-app/main/default/lwc/addBooks/addBooks.js
@@ -0,0 +1,50 @@
+// import { LightningElement, wire } from 'lwc';
+// import BOOKCHANNEL from '@salesforce/messageChannel/BooksChannel__c'
+// import { MessageContext, publish} from 'lightning/messageService';
+// export default class AddBooks extends LightningElement {
+//     @wire(MessageContext)
+//     context
+
+//    title = '';
+//     author = '';
+//     status = 'Not Read';
+
+//     statusOptions = [
+//         { label: 'Read', value: 'Read' },
+//         { label: 'Not Read', value: 'Not Read' }
+//     ];
+
+//     handleTitleChange(event) {
+//         this.title = event.target.value;
+//     }
+
+//     handleAuthorChange(event) {
+//         this.author = event.target.value;
+//     }
+
+//     handleStatusChange(event) {
+//         this.status = event.detail.value;
+//     }
+
+//     handleSubmit() {
+//         const message={
+//             title:{
+//                 value:this.title
+//             },
+//             author:{
+//                 value:this.author
+//             },
+//             status:{
+//                 value:this.status
+//             }
+//         }
+//         publish(this.context, BOOKCHANNEL, message)
+//         this.resetFormFields();
+//     }
+
+//     resetFormFields() {
+//         this.title = '';
+//         this.author = '';
+//         this.status = 'Not Read';
+//     }
+// }
