656bcfd651347c11a76ba64b49e4463bd80f53f1
kate <katejohn967@gmail.com>
2023-08-06 11:57:37 +0100

    Created 2 sibling components

--- force-app/main/default/lwc/allBooksList/allBooksList.js
+++ force-app/main/default/lwc/allBooksList/allBooksList.js
@@ -1,68 +1,68 @@
-// import { LightningElement, wire} from 'lwc';
-// import BOOKCHANNEL from '@salesforce/messageChannel/BooksChannel__c'
-// import { APPLICATION_SCOPE, MessageContext, subscribe} from 'lightning/messageService';
-// // const columns = [
-// //     { label: 'Title', fieldName: 'title' },
-// //     { label: 'Author', fieldName: 'author' },
-// //     { label: 'Status', fieldName: 'status' },
-// //     {
-// //         type: 'button',
-// //         initialWidth: 100,
-// //         typeAttributes: { label: 'Delete', variant: 'destructive', name: 'delete' },
-// //     },
-// // ];
+import { LightningElement, wire} from 'lwc';
+import BOOKCHANNEL from '@salesforce/messageChannel/BooksChannel__c'
+import { APPLICATION_SCOPE, MessageContext, subscribe} from 'lightning/messageService';
+// const columns = [
+//     { label: 'Title', fieldName: 'title' },
+//     { label: 'Author', fieldName: 'author' },
+//     { label: 'Status', fieldName: 'status' },
+//     {
+//         type: 'button',
+//         initialWidth: 100,
+//         typeAttributes: { label: 'Delete', variant: 'destructive', name: 'delete' },
+//     },
+// ];
 
-// export default class AllBooksList extends LightningElement {
+export default class AllBooksList extends LightningElement {
 
-//     receivedMessage
-//     subscription
+    receivedMessage
+    subscription
 
-//     @wire(MessageContext)
-//     context
+    @wire(MessageContext)
+    context
 
    
 
-//     // bookList = [];
-//     // columns = columns;
+    // bookList = [];
+    // columns = columns;
 
-//     // handleRowSelection(event) {
-//     //     const selectedRows = event.detail.selectedRows;
-//     //     const actionName = event.detail.action.name;
+    // handleRowSelection(event) {
+    //     const selectedRows = event.detail.selectedRows;
+    //     const actionName = event.detail.action.name;
 
-//     //     if (actionName === 'delete') {
-//     //         // Delete selected rows
-//     //         this.deleteSelectedRows(selectedRows);
-//     //     }
-//     // }
+    //     if (actionName === 'delete') {
+    //         // Delete selected rows
+    //         this.deleteSelectedRows(selectedRows);
+    //     }
+    // }
 
-//     // deleteSelectedRows(selectedRows) {
-//     //     const updatedBookList = [...this.bookList];
+    // deleteSelectedRows(selectedRows) {
+    //     const updatedBookList = [...this.bookList];
 
-//     //     selectedRows.forEach((selectedRow) => {
-//     //         const indexToRemove = updatedBookList.findIndex((book) => book.id === selectedRow.id);
-//     //         if (indexToRemove !== -1) {
-//     //             updatedBookList.splice(indexToRemove, 1);
-//     //             this.bookList = updatedBookList;
-//     //         }
-//     //     });
-//     // }
+    //     selectedRows.forEach((selectedRow) => {
+    //         const indexToRemove = updatedBookList.findIndex((book) => book.id === selectedRow.id);
+    //         if (indexToRemove !== -1) {
+    //             updatedBookList.splice(indexToRemove, 1);
+    //             this.bookList = updatedBookList;
+    //         }
+    //     });
+    // }
 
-//     connectedCallback() {
-//         this.handleFormSubmit()
-//     }
+    connectedCallback() {
+        this.handleFormSubmit()
+    }
 
-//     handleFormSubmit() {
-//         this.subscription = subscribe(this.context, BOOKCHANNEL, (message)=>{this.handleFormMessage(message)}, {scope:APPLICATION_SCOPE})
-//         // book.id = Date.now(); // Using Date.now() to create a unique identifier
-//         // this.bookList = [...this.bookList, book];
-//     }
+    handleFormSubmit() {
+        this.subscription = subscribe(this.context, BOOKCHANNEL, (message)=>{this.handleFormMessage(message)}, {scope:APPLICATION_SCOPE})
+        // book.id = Date.now(); // Using Date.now() to create a unique identifier
+        // this.bookList = [...this.bookList, book];
+    }
 
-//     handleFormMessage(message){
-//         this.receivedMessage = message.title.value ? message.title.value : "No message published"
-//     }
+    handleFormMessage(message){
+        this.receivedMessage = message.title.value ? message.title.value : "No message published"
+    }
    
 
-//     // disconnectedCallback() {
-//     //     window.removeEventListener('submit', this.handleFormSubmit);
-//     // }
-// }
+    // disconnectedCallback() {
+    //     window.removeEventListener('submit', this.handleFormSubmit);
+    // }
+}
