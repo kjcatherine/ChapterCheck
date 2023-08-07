3945fc24fc12f3b34cc58f757170e6ca30729eee
kate <katejohn967@gmail.com>
2023-08-07 11:58:47 +0100

    Working on the form component

--- force-app/main/default/lwc/addBooks/addBooks.js
+++ force-app/main/default/lwc/addBooks/addBooks.js
@@ -1,50 +1,50 @@
-// import { LightningElement, wire } from 'lwc';
-// import BOOKCHANNEL from '@salesforce/messageChannel/BooksChannel__c'
-// import { MessageContext, publish} from 'lightning/messageService';
-// export default class AddBooks extends LightningElement {
-//     @wire(MessageContext)
-//     context
+import { LightningElement, wire } from 'lwc';
+import BOOKCHANNEL from '@salesforce/messageChannel/BooksChannel__c'
+import { MessageContext, publish} from 'lightning/messageService';
+export default class AddBooks extends LightningElement {
+    @wire(MessageContext)
+    context
 
-//    title = '';
-//     author = '';
-//     status = 'Not Read';
+   title = '';
+    author = '';
+    status = 'Not Read';
 
-//     statusOptions = [
-//         { label: 'Read', value: 'Read' },
-//         { label: 'Not Read', value: 'Not Read' }
-//     ];
+    statusOptions = [
+        { label: 'Read', value: 'Read' },
+        { label: 'Not Read', value: 'Not Read' }
+    ];
 
-//     handleTitleChange(event) {
-//         this.title = event.target.value;
-//     }
+    handleTitleChange(event) {
+        this.title = event.target.value;
+    }
 
-//     handleAuthorChange(event) {
-//         this.author = event.target.value;
-//     }
+    handleAuthorChange(event) {
+        this.author = event.target.value;
+    }
 
-//     handleStatusChange(event) {
-//         this.status = event.detail.value;
-//     }
+    handleStatusChange(event) {
+        this.status = event.detail.value;
+    }
 
-//     handleSubmit() {
-//         const message={
-//             title:{
-//                 value:this.title
-//             },
-//             author:{
-//                 value:this.author
-//             },
-//             status:{
-//                 value:this.status
-//             }
-//         }
-//         publish(this.context, BOOKCHANNEL, message)
-//         this.resetFormFields();
-//     }
+    handleSubmit() {
+        const message={
+            title:{
+                value:this.title
+            },
+            author:{
+                value:this.author
+            },
+            status:{
+                value:this.status
+            }
+        }
+        publish(this.context, BOOKCHANNEL, message)
+        this.resetFormFields();
+    }
 
-//     resetFormFields() {
-//         this.title = '';
-//         this.author = '';
-//         this.status = 'Not Read';
-//     }
-// }
+    // resetFormFields() {
+    //     this.title = '';
+    //     this.author = '';
+    //     this.status = 'Not Read';
+    // }
+}
